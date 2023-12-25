1ae94923b26c106362923f76778529da68c090c4
AdriSau <107517286+AdriSau@users.noreply.github.com>
2023-12-25 15:17:21 +0100

    changes

---
+++ Object_Detection_main.py
@@ -0,0 +1,8 @@
+
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
+
+# Use the model
+model.train(data="D:\ProyectosGithub\Multiple-Object-Detection-and-Tracking-\Sources\data.yaml", epochs=3)  # train the model
