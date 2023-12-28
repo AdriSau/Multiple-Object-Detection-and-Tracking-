e60c35536c2400c2b1c763df897123df0c8c44c2
AdriSau <107517286+AdriSau@users.noreply.github.com>
2023-12-28 03:00:00 +0100

    cm
    T15 and t39, RELEVANT

--- Object_Detection_main.py
+++ Object_Detection_main.py
@@ -1,8 +1,11 @@
-
+import ultralytics
 from ultralytics import YOLO
+import torch
 
-# Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
+print(torch.cuda.is_available())
+print(torch.version.cuda)
+ultralytics.checks()
 
-# Use the model
-model.train(data="D:\ProyectosGithub\Multiple-Object-Detection-and-Tracking-\Sources\data.yaml", epochs=40)  # train the model
+if __name__ == '__main__':
+    model = YOLO("yolov8n.yaml")  # build a new model from scratch
+    model.train(data="D:\ProyectosGithub\Multiple-Object-Detection-and-Tracking-\Sources\data.yaml", epochs=2)  # train the model
