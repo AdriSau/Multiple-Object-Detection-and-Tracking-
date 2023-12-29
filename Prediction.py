47d6d1a9bcd4a543281f4b5900c1d8010a6da787
AdriSau <adrian.saugar@alumnos.upm.es>
2023-12-29 13:08:42 +0100

    tracking

--- Prediction.py
+++ Prediction.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 import cv2
 model = YOLO('best.pt')
 
-results = model.predict(source="0", show=True)
-
+results = model.predict(source="1", show=True, conf = 0.45, classes = 0)
+#results = model.track(source = "1", show=True,conf = 0.45, classes = 0, tracker = "bytetrack.yaml" )
 print(results)
