6718d1e43bfedbe8dad112cf44befedd10c58101
AdriSau <107517286+AdriSau@users.noreply.github.com>
2023-12-30 14:05:53 +0100

    cm

--- Prediction.py
+++ Prediction.py
@@ -2,6 +2,8 @@ from ultralytics import YOLO
 import cv2
 model = YOLO('best.pt')
 
+highway = "testingFootage/prueba.mp4"
+tCircle = "testingFootage/traffic_circle_footage.mp4"
 #results = model.predict(source="1", show=True, conf = 0.45, classes = 0)
-results = model.track(source = "1",show=True,conf = 0.45, classes = 0, tracker = "bytetrack.yaml" )
+results = model.track(source = highway,show=True,conf = 0.3, classes = 0, tracker = "bytetrack.yaml")
 print(results)
