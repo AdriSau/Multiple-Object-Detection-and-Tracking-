a78dd2a9fb36250db445ca49b32a67893a5c4076
AdriSau <adrian.saugar@alumnos.upm.es>
2024-02-14 16:36:50 +0100

    cm

--- Prediction.py
+++ Prediction.py
@@ -5,6 +5,10 @@ model = YOLO ('2000_img.pt')
 
 highway = "testingFootage/prueba.mp4"
 tCircle = "testingFootage/traffic_circle_footage.mp4"
+germanyFT_1 = "testingFootage/Video_1_germany.MOV"
+germanyFT_2 = "testingFootage/Video_2_germany.MOV"
+germanyFT_3 = "testingFootage/Video_3_germany.MOV"
+
 #results = model.predict(source="1", show=True, conf = 0.45, classes = 0)
-results = model.track(source = tCircle,show=True,conf = 0.3, classes = 0, tracker = "bytetrack.yaml")
+results = model.track(source = highway,show=True,conf = 0.25, tracker = "bytetrack.yaml")
 print(results)
