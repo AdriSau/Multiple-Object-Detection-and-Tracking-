9e3cf4df27ae391780a2196bb92ab34237c598ee
AdriSau <adrian.saugar@alumnos.upm.es>
2024-03-04 14:26:54 +0100

    cm

--- Yolo_Prediction.py
+++ Yolo_Prediction.py
@@ -11,10 +11,10 @@ germanyFT_1 = "testingFootage/Clip_TopView_720p.mp4"
 image_all = "testingFootage/ALL_img.jpg"
 image_footbal = "testingFootage/football_field.jpg"
 
-results = model.predict(source= image_all, show_conf = True, save = True)
-results = model.predict(source= image_footbal, show_conf = True, save = True)
-results = model_car.predict(source= image_all, show_conf = True, save = True)
-results = model_car.predict(source= image_footbal, show_conf = True, save = True)
+#results = model.predict(source= image_all, show_conf = True, save = True)
+#results = model.predict(source= image_footbal, show_conf = True, save = True)
+#results = model_car.predict(source= image_all, show_conf = True, save = True)
+#results = model_car.predict(source= image_footbal, show_conf = True, save = True)
 #results = model.track(source = highway, show = True, tracker = "bytetrack.yaml")
-results = model.track(source= "0", save = True, conf = 0.3, classes = 1, show = True)
+results = model.track(source= "0", save = True, conf = 0.4, show = True)
 print(results)
